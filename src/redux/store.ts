2d71fa014dc6a9b124e420076db518e63a9e203f
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-06-30 16:14:42 -0500

    feat: add the service to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counter/counterSlice'
 
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { apiSlice } from './api/apiSlice'
+
 export const store = configureStore({
   reducer: {
-    counter: counterSlice
+    counter: counterSlice,
+    api: apiSlice.reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
+setupListeners(store.dispatch)
 export type AppDispatch = typeof store.dispatch
